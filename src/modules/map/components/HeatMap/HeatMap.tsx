bb5eb22fcb9df12c6bf51e995d12a1fdf1747fa5
Diana Navarro <navarroruiz.diana@gmail.com>
2022-09-02 13:53:30 -0500

    feat: include integration with google maps

---
+++ src/modules/map/components/HeatMap/HeatMap.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import GoogleMap from './GoogleMap';
+import GoogleMarker from './GoogleMarker';
+
+const HeatMap: React.FC = () => {
+  const [clicks, setClicks] = React.useState<google.maps.LatLng[]>([]);
+  const [zoom, setZoom] = React.useState(12);
+  const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
+    lat: 0,
+    lng: 0,
+  });
+
+  const onClick = (e: google.maps.MapMouseEvent) => {
+    // avoid directly mutating state
+    setClicks([...clicks, e.latLng!]);
+  };
+
+  const onIdle = (m: google.maps.Map) => {
+    setZoom(m.getZoom()!);
+    setCenter(m.getCenter()!.toJSON());
+  };
+
+  return (
+    <GoogleMap
+      center={center}
+      onClick={onClick}
+      onIdle={onIdle}
+      zoom={zoom}
+      style={{ flexGrow: '1', height: '100%' }}
+    >
+      {clicks.map((latLng, i) => (
+        <GoogleMarker key={i} position={latLng} />
+      ))}
+    </GoogleMap>
+  );
+};
+export default HeatMap;
