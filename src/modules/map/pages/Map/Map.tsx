7f7820158d7d5bd3e37dea91a7c052bb7f290482
Diana Navarro <navarroruiz.diana@gmail.com>
2022-09-09 16:38:34 -0500

    feat: integrate crimes on map

--- src/modules/map/pages/Map/Map.tsx
+++ src/modules/map/pages/Map/Map.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../../../../components/Button/Button';
 import Input from '../../../../components/Input/Input';
 import Card from '../../../../components/Card/Card';
 import MainLayout from '../../../../components/MainLayout/MainLayout';
+import Loading from '../../../../components/Loading/Loading';
+import { useApi } from '../../../../hooks/useApi';
+import { getCrimes } from '../../../../services/crime.service';
 import HeatMap from '../../components/HeatMap/HeatMap';
 import './map.scss';
 
 const Map: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [crimes, setCrimes] = useState<any>(null);
+  const [responseEndpoint, callEndpoint] = useApi();
+
+  useEffect(() => {
+    if (loading && responseEndpoint?.data) {
+      setLoading(false);
+      setCrimes(responseEndpoint?.data);
+    } else if (!loading && responseEndpoint?.data === null) {
+      setLoading(true);
+      callEndpoint(getCrimes());
+    }
+  }, [responseEndpoint]);
+
   return (
     <MainLayout className="MapPage">
+      {loading && <Loading />}
       <section className="MapPage__Container">
-        <HeatMap />
+        {crimes && <HeatMap crimes={crimes} />}
         <Card className="MapPage__Container__Filters">
           <Input placeholder="Fecha"></Input>
           <Input placeholder="Hora"></Input>
