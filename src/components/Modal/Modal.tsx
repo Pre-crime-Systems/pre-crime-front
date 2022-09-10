d19680b11cd8621dd840ca9a91b23fe4d565063d
Diana Navarro <navarroruiz.diana@gmail.com>
2022-09-09 22:52:01 -0500

    feat: improve Modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -6,10 +6,11 @@ interface IModalProps {
   active: boolean;
   children?: React.ReactNode;
   onClose?: any;
+  title: string;
 }
 
 const Modal: React.FC<IModalProps> = (props: IModalProps) => {
-  const { active, onClose, children } = props;
+  const { active, children, onClose, title } = props;
 
   const handlerModal = (event: any) => {
     const elementModal = event.target.closest('.modal');
@@ -28,7 +29,17 @@ const Modal: React.FC<IModalProps> = (props: IModalProps) => {
 
   return (
     <article className={`modal ${active ? 'modal--show' : ''}`}>
-      <section className="modal__content">{children}</section>
+      <section className="modal__content">
+        <div className="modalHeader">
+          <h3 className="modalHeader__title">{title}</h3>
+          {onClose && (
+            <button className="modalHeader__close" onClick={onClose}>
+              close
+            </button>
+          )}
+        </div>
+        {children}
+      </section>
     </article>
   );
 };
