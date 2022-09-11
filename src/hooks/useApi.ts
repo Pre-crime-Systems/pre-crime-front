0a210e09678c4c15faf6d54271a3f3996367fc0c
Diana Navarro <navarroruiz.diana@gmail.com>
2022-09-11 15:33:26 -0500

    feature: improve map.data style and click

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ApiMethod, ApiRequest, ApiResponse } from '../models/api.model';
+import { RoutePaths } from '../routes/routePaths';
 import * as localStorage from '../utils/localStorage.util';
 
 export const useApi = (): [
@@ -8,6 +10,7 @@ export const useApi = (): [
 ] => {
   const initResponse: ApiResponse = { data: null, error: false };
   const [response, setResponse] = useState<ApiResponse>(initResponse);
+  const navigate = useNavigate();
 
   const callEndpoint = async ({ method, url, body }: ApiRequest) => {
     setResponse(initResponse);
@@ -26,7 +29,12 @@ export const useApi = (): [
 
       const fetchResponse = await fetch(url, request);
       const fetchJson = await fetchResponse.json();
-      setResponse({ data: fetchJson, error: false });
+      if (fetchJson?.error) {
+        console.error('[ERROR API]', fetchJson?.error);
+        navigate(RoutePaths.Login);
+      } else {
+        setResponse({ data: fetchJson, error: false });
+      }
     } catch (error) {
       console.error(error);
       setResponse({ data: null, error: true });
--- src/modules/map/components/CrimeMap/CrimeMap.tsx
+++ src/modules/map/components/CrimeMap/CrimeMap.tsx
@@ -5,7 +5,12 @@ import {
   useJsApiLoader,
 } from '@react-google-maps/api';
 import { Libraries } from '@react-google-maps/api/dist/utils/make-load-script-url';
-import { API_KEY } from '../../../../constants/google.constant';
+import {
+  API_KEY,
+  HEATMAP_GRADIENT,
+  HEATMAP_RADIUS,
+  LIMA_GEOJSON_URL,
+} from '../../../../constants/google.constant';
 const libraries: Libraries = ['visualization'];
 
 interface CrimeMapProps {
@@ -14,11 +19,11 @@ interface CrimeMapProps {
 
 const CrimeMap: React.FC<CrimeMapProps> = (props: CrimeMapProps) => {
   const { crimes } = props;
+
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: API_KEY,
     libraries,
   });
-  const zoom = 12;
   const center = { lat: -12.0874512, lng: -77.0499421 };
   const mapOptions = {
     mapTypeControl: false,
@@ -26,6 +31,12 @@ const CrimeMap: React.FC<CrimeMapProps> = (props: CrimeMapProps) => {
     streetViewControl: false,
     zoomControl: false,
   };
+  const mapStyle = {
+    flexGrow: '1',
+    height: '100%',
+    borderRadius: '0.5rem',
+  };
+  const zoom = 12;
 
   const contentString =
     '<div id="content">' +
@@ -33,67 +44,45 @@ const CrimeMap: React.FC<CrimeMapProps> = (props: CrimeMapProps) => {
     '</div>' +
     '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
     '<div id="bodyContent">' +
-    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    'sandstone rock formation in the southern part of the ' +
-    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-    'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-    'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-    'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-    'Aboriginal people of the area. It has many springs, waterholes, ' +
-    'rock caves and ancient paintings. Uluru is listed as a World ' +
-    'Heritage Site.</p>' +
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-    'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-    '(last visited June 22, 2009).</p>' +
+    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large </p>' +
     '</div>' +
     '</div>';
 
   const onLoadMap = (map: any) => {
-    map.data.loadGeoJson(
-      'https://raw.githubusercontent.com/joseluisq/peru-geojson-datasets/master/lima_callao_distritos.geojson'
-    );
+    map.data.loadGeoJson(LIMA_GEOJSON_URL);
     map.data.setStyle((feature: any) => {
-      const ascii = feature.getProperty('ascii');
-      const color = ascii > 91 ? 'red' : 'transparent';
+      const provi = feature.getProperty('provincia');
+      const color = provi === 'LIMA' ? 'red' : 'blue';
       return {
         fillColor: color,
         strokeWeight: 1,
         strokeColor: '#024481',
       };
     });
+
+    let prevInfo: any = null;
     map.data.addListener('click', function (event: any) {
-      map.data.overrideStyle(event.feature, { fillColor: 'red' });
-      const infowindow = new google.maps.InfoWindow({
+      //set info
+      const infoWindow = new google.maps.InfoWindow({
         content: contentString,
         position: event.latLng,
       });
-      infowindow.open({
+      if (prevInfo !== null && prevInfo !== infoWindow) {
+        prevInfo?.close();
+      }
+      //show info
+      infoWindow.open({
         map,
         shouldFocus: false,
       });
+      //save values
+      prevInfo = infoWindow;
     });
   };
 
   const onLoadHeatMap = (heatmapLayer: any) => {
-    const gradient = [
-      'rgba(0, 255, 255, 0)',
-      'rgba(0, 255, 255, 1)',
-      'rgba(0, 191, 255, 1)',
-      'rgba(0, 127, 255, 1)',
-      'rgba(0, 63, 255, 1)',
-      'rgba(0, 0, 255, 1)',
-      'rgba(0, 0, 223, 1)',
-      'rgba(0, 0, 191, 1)',
-      'rgba(0, 0, 159, 1)',
-      'rgba(0, 0, 127, 1)',
-      'rgba(63, 0, 91, 1)',
-      'rgba(127, 0, 63, 1)',
-      'rgba(191, 0, 31, 1)',
-      'rgba(255, 0, 0, 1)',
-    ];
-    heatmapLayer.set('gradient', gradient);
-    heatmapLayer.set('radius', 20);
+    heatmapLayer.set('gradient', HEATMAP_GRADIENT);
+    heatmapLayer.set('radius', HEATMAP_RADIUS);
   };
 
   const parserData = () => {
@@ -105,11 +94,7 @@ const CrimeMap: React.FC<CrimeMapProps> = (props: CrimeMapProps) => {
   return isLoaded ? (
     <GoogleMap
       center={center}
-      mapContainerStyle={{
-        flexGrow: '1',
-        height: '100%',
-        borderRadius: '0.5rem',
-      }}
+      mapContainerStyle={mapStyle}
       onLoad={onLoadMap}
       options={mapOptions}
       zoom={zoom}
