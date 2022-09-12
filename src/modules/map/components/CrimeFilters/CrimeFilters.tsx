ffbba9b36e58c2f58379fffadc63c269c84ab650
Diana Navarro <navarroruiz.diana@gmail.com>
2022-09-12 09:40:55 -0500

    feat: integrate districts and zones

--- src/modules/map/components/CrimeFilters/CrimeFilters.tsx
+++ src/modules/map/components/CrimeFilters/CrimeFilters.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cx from 'classnames';
 import Button from '../../../../components/Button/Button';
-import Input from '../../../../components/Input/Input';
 import Card from '../../../../components/Card/Card';
+import Input from '../../../../components/Input/Input';
+import Select from '../../../../components/Select/Select';
+import Switch from '../../../../components/Switch/Switch';
+import { useApi } from '../../../../hooks/useApi';
+import {
+  getDistricts,
+  getZonesByDistrict,
+} from '../../../../services/location.service';
 import './crimeFilters.scss';
 
 interface CrimeFiltersProps {
@@ -13,13 +20,78 @@ const CrimeFilters: React.FC<CrimeFiltersProps> = (
   props: CrimeFiltersProps
 ) => {
   const { className } = props;
+  const [districts, setDistricts] = useState<any>(null);
+  const [loadingDistricts, setLoadingDistricts] = useState<any>(null);
+  const [selectedDistrict, setSelectedDistrict] = useState<any>(null);
+  const [zones, setZones] = useState<any>(null);
+  const [loadingZones, setLoadingZones] = useState<any>(null);
+  const [selectedZone, setSelectedZone] = useState<any>(null);
+  const [districtsResponse, callDistricts] = useApi();
+  const [zonesResponse, callZones] = useApi();
+
+  useEffect(() => {
+    if (loadingDistricts && districtsResponse?.data) {
+      setLoadingDistricts(false);
+      setDistricts(
+        districtsResponse?.data?.map((district: any) => {
+          return {
+            label: district?.name,
+            value: district?.id,
+          };
+        })
+      );
+    } else if (!loadingDistricts && districtsResponse?.data === null) {
+      setLoadingDistricts(true);
+      callDistricts(getDistricts());
+    }
+  }, [districtsResponse]);
+
+  useEffect(() => {
+    if (loadingZones && zonesResponse?.data) {
+      setLoadingZones(false);
+      setZones(
+        zonesResponse?.data?.map((zone: any) => {
+          return {
+            label: zone?.code,
+            value: zone?.id,
+          };
+        })
+      );
+    }
+  }, [zonesResponse]);
 
   return (
     <Card className={cx('crimeFilters', className && className)}>
-      <Input placeholder="Fecha"></Input>
-      <Input placeholder="Hora"></Input>
-      <Input placeholder="Dirección"></Input>
-      <Button buttonType="secondary">Filtrar</Button>
+      <h3 className="crimeFilters__title">Filtros</h3>
+      <Switch label="Modo predicción" />
+      <section className="crimeFilters__fields">
+        <Select
+          label="Distrito"
+          placeholder="Distrito"
+          options={districts}
+          value={selectedDistrict}
+          onChange={(newValue) => {
+            setSelectedDistrict(newValue);
+            setSelectedZone(null);
+            if (newValue) {
+              setLoadingZones(true);
+              callZones(getZonesByDistrict(newValue?.value));
+            }
+          }}
+        />
+        <Select
+          label="Código postal"
+          placeholder="Código postal"
+          options={zones}
+          value={selectedZone}
+          onChange={(newValue) => {
+            setSelectedZone(newValue);
+          }}
+        />
+        <Input label="Fecha" placeholder="Fecha" disabled={true}></Input>
+        <Input label="Hora" placeholder="Hora" disabled={true}></Input>
+        <Button buttonType="secondary">Filtrar</Button>
+      </section>
     </Card>
   );
 };
---
+++ src/services/location.service.ts
@@ -0,0 +1,18 @@
+import { BASE_URL } from '../constants/api.constant';
+import { ApiMethod, ApiRequest } from '../models/api.model';
+
+export const getDistricts = () => {
+  const request: ApiRequest = {
+    method: ApiMethod.Get,
+    url: `${BASE_URL}/api/districts`,
+  };
+  return request;
+};
+
+export const getZonesByDistrict = (districtId: number) => {
+  const request: ApiRequest = {
+    method: ApiMethod.Get,
+    url: `${BASE_URL}/api/districts/${districtId}/zones`,
+  };
+  return request;
+};
